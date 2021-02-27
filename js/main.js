5af5fb46920ad836acd79a67c5c3114797f50e62
srg <skabass@yandex.ru>
2021-02-28 02:37:47 +0300

    Добавляет утилитарные функции

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * @param {number} num1
  * @param {number} num2
  */
-const getRandomInteger = (num1, num2) => {
+const getRandomInteger = (num1, num2 = 0) => {
   if (num1 === num2) {
     return num1;
   }
@@ -19,3 +19,28 @@ const getRandomInteger = (num1, num2) => {
 const checkStrLength = (str, maxLength) => {
   return (str.length <= maxLength) ? true : false;
 };
+
+/**
+ * @param {number} minNum
+ * @param {number} maxNum
+ */
+const getArrayDontRepeatNum = (minNum, maxNum) => {
+  const length = maxNum - minNum + 1;
+  const setNum = new Set;
+
+  while (setNum.size < length) {
+    setNum.add(getRandomInteger(minNum, maxNum));
+  }
+
+  return [...setNum];
+};
+
+/**
+ * @param {Array} arr
+ */
+const makeRandomArr = (arr) => [...arr].sort(() => Math.random() - 0.5);
+
+/**
+ * @param {number} max
+ */
+const createArrNaturalNum = (max) => [...(Array(max).keys())].map(item => item + 1);
